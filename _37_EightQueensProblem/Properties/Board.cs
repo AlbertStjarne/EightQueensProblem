9d6c10075908230dd09eb240e34bb30384c6a40a
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 19:04:51 +0100

    adds Board class with the states - without queens and with all queens placed out, adds method to display the board

---
+++ _37_EightQueensProblem/Properties/Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _37_EightQueensProblem.Properties
+{
+    public class Board
+    {
+        public const char BoardChar = '.';
+        public const char QueenChar = 'X';
+        public char[,] board;
+
+        public Board()
+        {
+            board = new char[8, 8];
+        }
+
+        public void DisplayBoard()
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                for (var c = 0; c < 8; c++)
+                {
+                    Console.Write(board[i, c] + "  ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        public void Init()
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                for (var c = 0; c < 8; c++)
+                {
+                    board[i, c] = BoardChar;
+                }
+            }
+        }
+
+
+    }
+}
