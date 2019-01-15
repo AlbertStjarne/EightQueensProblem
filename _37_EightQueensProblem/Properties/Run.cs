fe819c8aed2270bb3ddc5b5e31efb72a6ab63b62
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 19:19:35 +0100

    adds Run class that puts the queen on the board, adds method that check placed queen is ok row wise

---
+++ _37_EightQueensProblem/Properties/Run.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _37_EightQueensProblem.Properties
+{
+    public class Run
+    {
+        private Board _board;
+        private int _runCounter;  //count attempts til finish
+        private int _queensCounter;
+        private bool[] _usedRows;  //check if one row doesn't have more than one queen
+        private static Random _rand;
+
+        public Run()
+        {
+            _rand = new Random();
+            _board = new Board();
+            _usedRows = new bool[8];
+            _runCounter = 0;
+            _queensCounter = 0;
+        }
+
+        public void SetPositions()
+        {
+            var col = 0;
+            var row = 0;
+
+            while (_queensCounter < 8)
+            {
+                row = _rand.Next(0, 8);
+                if (IsValidRow(row))
+                {
+                    _board.board[row, col] = Board.QueenChar;
+                    col++;
+                    _queensCounter++;
+                }
+            }
+            _runCounter++;
+        }
+
+        public bool IsValidRow(int row)
+        {
+            if (!_usedRows[row])
+            {
+                _usedRows[row] = true;
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
