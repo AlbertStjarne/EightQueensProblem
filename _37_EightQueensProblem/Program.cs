9dba3d6a0ce6055daadc9634cdafd43d0cc7d932
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 18:51:12 +0100

    initializes project and adds .gitignore file

---
+++ _37_EightQueensProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _37_EightQueensProblem
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
